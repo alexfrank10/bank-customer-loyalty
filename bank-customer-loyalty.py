b68d5343cc5662b3f9e5367781d2f0f1e8307c6d
alexfrank10 <alexiano5161@gmail.com>
2022-03-19 00:44:08 +0100

    first commit

---
+++ bank-customer-loyalty.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+
+
+# import dataset
+df = pd.read_csv('dataset/Churn_Modelling.csv')
+
